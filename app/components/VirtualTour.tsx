c5c30dd9b1217eb2c7c3f6582ddbcf565bcfd0fc
Cornelius James A. Lasala <lasalabambam9@gmail.com>
2026-09-08 11:17:37 +0800

    fix(virtual-tour): set default zoom level to fully zoomed out

--- app/components/VirtualTour.tsx
+++ app/components/VirtualTour.tsx
@@ -14,6 +14,9 @@ export default function VirtualTour({ image }: VirtualTourProps) {
       height="100%"
       width="100%"
       littlePlanet={false}
+      defaultZoomLvl={0} // 0 = maximum zoom out, 50 = default middle, 100 = maximum zoom in
+      minFov={30}        // optional: limit how far the user can zoom IN
+      maxFov={90}        // optional: limit how far the user can zoom OUT
       navbar={['zoom', 'move', 'download', 'fullscreen']}
     />
   );
